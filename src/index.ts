167a26a9e5f0f66364b753f938012ab38fe61262
thondery <thondery@163.com>
2019-06-27 11:57:59 +0800

    renew project.

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { RouterMethods, RouterController, ControllerMount, Router, Filter } from './router'
-export { ExpressError, ErrorSetting } from './error'
-export { Middleware, MiddlewareSetting, RegisterMiddlewareMethod } from './middleware'
